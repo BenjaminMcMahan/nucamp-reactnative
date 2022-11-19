847a7a8a500053c58a8464ecc3fc4843a1d3c7c6
Benjamin McMahan <108589431+BenjaminMcMahan@users.noreply.github.com>
2022-11-19 11:58:23 -0500

    Week 3 Workshop Assignment

--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,1 +1,1 @@
-export const baseUrl = 'http://192.168.0.23:3001/'; // TODO - why are we setting a specific IP?
+export const baseUrl = 'http://192.168.1.194:3001/'; // TODO - why are we setting a specific IP?
